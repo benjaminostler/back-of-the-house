079ef42a1cfabbe9fc788e4e173a69b0642a6be7
Edward Lee <eleet94@gmail.com>
2023-07-12 22:18:01 +0000

    changed table name form "menu" to menu_item", added price parameter

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from routers import reservations, menu
+from routers import accounts, menu_item
 
 app = FastAPI()
-app.include_router(reservations.router)
-app.include_router(menu.router)
+app.include_router(accounts.router)
+app.include_router(menu_item.router)
