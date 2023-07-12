d140b6111f364146924b32e5da504dfaeaaee833
Ed Lee <eleet94@gmail.com>
2023-07-11 21:01:43 -0700

    added price to menu, changed variable names to better suit project's scope

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from routers import accounts, menu
+from routers import accounts, menu_item
 
 app = FastAPI()
 app.include_router(accounts.router)
-app.include_router(menu.router)
+app.include_router(menu_item.router)
