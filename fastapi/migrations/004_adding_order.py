e50b7f487090ad4879311a6e92a6949696574b40
Raymond <quachraymond82@gmail.com>
2023-07-13 15:53:40 -0700

    added order and invoice table

--- fastapi/migrations/004_adding_order.py
+++ fastapi/migrations/004_adding_order.py
@@ -2,13 +2,13 @@ steps = [
     [
         ## Create the table
         """
-        CREATE TABLE order (
+        CREATE TABLE orders (
             id SERIAL PRIMARY KEY NOT NULL,
             menu_item_id SERIAL NOT NULL,
             category VARCHAR(50) NOT NULL,
             quantity VARCHAR (20) NOT NULL,
             price_usd NUMERIC (6,2) NOT NULL
-         );
+        );
         """,
         ## Drop the table
         """
@@ -16,26 +16,3 @@ steps = [
         """,
     ]
 ]
-            # mozzarella_sticks BOOL,
-            # disco_fries BOOL,
-            # buffalo_wings BOOL,
-            # onion_rings BOOL,
-            # artichoke_dip BOOL,
-            # coke BOOL,
-            # green_tea BOOL,
-            # coffee BOOL,
-            # cold_brew BOOL,
-            # vanilla_milkshake BOOL,
-            # chocolate_milkshake BOOL,
-            # oreo_milkshake BOOL,
-            # cheeseburger BOOL,
-            # double_cheeseburger BOOL,
-            # grilled_cheese BOOL,
-            # chicken_club BOOL,
-            # chicken_tenders BOOL,
-            # tuna_melt BOOL,
-            # fries BOOL,
-            # house_salad BOOL,
-            # cherry_pie BOOL,
-            # key_lime_pie BOOL,
-            # vanilla_gelato BOOL
