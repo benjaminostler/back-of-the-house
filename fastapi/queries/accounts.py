65cee33c07a218594ad0ebca1564ac159d9d857b
ken <kenny.phung12@gmail.com>
2023-07-17 23:01:05 +0000

    Minor changes to accounts.py in queries and routers.

--- fastapi/queries/accounts.py
+++ fastapi/queries/accounts.py
@@ -1,7 +1,6 @@
 from pydantic import BaseModel
-from typing import List, Union
+from typing import Optional, List, Union
 from queries.pool import pool
-
 
 class Error(BaseModel):
     message: str
@@ -9,19 +8,11 @@ class Error(BaseModel):
 class DuplicateAccountError(ValueError):
     pass
 
-class DuplicateAccountError(ValueError):
-    pass
-
-
-class DuplicateAccountError(ValueError):
-    pass
-
-
 class AccountIn(BaseModel):
     first_name: str
     last_name: str
     username: str
-    hashed_password: str
+    password: str
     email: str
     phone_number: str
 
@@ -54,60 +45,6 @@ class AccountRepository:
                             , email
                             , phone_number
                         FROM accounts
-                        WHERE username =%s
-                        """,
-                        [username],
-                    )
-                    record = result.fetchone()
-                    return self.record_to_account(record)
-        except Exception as e:
-            print(e)
-            raise ValueError("Could not get account") from e
-
-    def update(self, id: int, hashed_password: str, accounts: AccountIn) -> Union[AccountOut, Error]:
-        try:
-            with pool.connection() as conn:
-                with conn.cursor() as db:
-                    db.execute(
-                        """
-                        UPDATE accounts
-                        SET first_name = %s
-                            , last_name = %s
-                            , username = %s
-                            , hashed_password = %s
-                            , email = %s
-                            , phone_number = %s
-                        WHERE id = %s
-                        """,
-                        [
-                            accounts.first_name,
-                            accounts.last_name,
-                            accounts.username,
-                            accounts.hashed_password,
-                            accounts.email,
-                            accounts.phone_number,
-                            id,
-                        ]
-                    )
-                    return AccountOut(
-                        id=id,
-                        first_name=accounts.first_name,
-                        last_name=accounts.last_name,
-                        username=accounts.username,
-                        email=accounts.email,
-                        phone_number=accounts.phone_number
-                    )
-        except Exception as e:
-            print(e)
-            return {"message": "Could not update account"}
-
-    def get_all(self) -> Union[Error, List[AccountOut]]:
-        try:
-            with pool.connection() as conn:
-                with conn.cursor() as db:
-                    db.execute(
-                        """
-                        SELECT * FROM accounts
                         WHERE username =%s
                         """,
                         [username],
@@ -171,7 +108,7 @@ class AccountRepository:
                             first_name=entry[1],
                             last_name=entry[2],
                             username=entry[3],
-                            hashed_password=entry[4],
+                            password=entry[4],
                             email=entry[5],
                             phone_number=entry[6],
                         )
@@ -180,61 +117,6 @@ class AccountRepository:
         except Exception:
             return {"message": "Could not get all accounts"}
 
-    def create(self, info: AccountIn, hashed_password: str) -> AccountOutWithPassword:
-        with pool.connection() as conn:
-            with conn.cursor() as db:
-                result = db.execute(
-                    """
-                    INSERT INTO accounts
-                        (first_name, last_name, username, hashed_password, email, phone_number)
-                    VALUES
-                        (%s, %s, %s, %s, %s, %s)
-                    RETURNING id, email, hashed_password;
-                    """,
-                    [
-                        info.first_name,
-                        info.last_name,
-                        info.username,
-                        hashed_password,
-                        info.email,
-                        info.phone_number
-                    ]
-                )
-                id = result.fetchone()[0]
-                return AccountOutWithPassword(
-                    id=id,
-                    first_name=info.first_name,
-                    last_name=info.last_name,
-                    username=info.username,
-                    hashed_password=hashed_password,
-                    email=info.email,
-                    phone_number=info.phone_number
-                )
-
-    def record_to_account(self, record) -> AccountOutWithPassword:
-        return AccountOutWithPassword(
-            id=record[0],
-            first_name=record[1],
-            last_name=record[2],
-            username=record[3],
-            hashed_password=record[4],
-            email=record[5],
-            phone_number=record[6],
-        )
-        # account_dict = {
-        #     "id": record[0],
-        #     "first_name": record[1],
-        #     "last_name": record[2],
-        #     "username": record[3],
-        #     "hashed_password": record[4],
-        #     "email": record[5],
-        #     "phone_number": record[6]
-        # }
-        # return account_dict
-
-    def account_in_to_out(self, id: int, accounts: AccountIn):
-        old_data = accounts.dict()
-        return AccountOut(id=id, **old_data)
     def create(self, info: AccountIn, hashed_password: str) -> AccountOutWithPassword:
         with pool.connection() as conn:
             with conn.cursor() as db:
