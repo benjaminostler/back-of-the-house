5264ad28332dbaecf19a8c82c9633175e9339e1f
Raymond <quachraymond82@gmail.com>
2023-07-18 09:58:41 -0700

    created order items, added some items in journal

--- fastapi/routers/invoices.py
+++
@@ -1,7 +0,0 @@
-
-@router.get("/invoices/{invoice_id}", response_model=Optional[InvoiceOut])
-def get_one_invoice(
-        invoice_id: int,
-        repo: InvoiceRepository = Depends(),
-) -> InvoiceOut:
-    return repo.get_one(invoice_id)
