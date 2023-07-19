faaa38dad5e2bacc3779446e61c79fe5d81316a5
Benjamin Ostler <benjaminostler@gmail.com>
2023-07-19 14:44:28 -0700

    Updating outdated files

--- fastapi/routers/menu_items.py
+++ fastapi/routers/menu_items.py
@@ -11,14 +11,14 @@ from queries.menu_items import (
 router = APIRouter()
 
 
-@router.post("/menu_item", response_model=Union[MenuItemOut, Error], tags=["Menu Items"])
+@router.post("/menu_item", response_model=Union[MenuItemsOut, Error], tags=["Menu Items"])
 def create_menu_item(
     menu_items: MenuItemsIn, repo: MenuItemsRepository = Depends()
 ):
     return repo.create(menu_items)
 
 
-@router.get("/menu_item", response_model=Union[Error, List[MenuItemOut]], tags=["Menu Items"])
+@router.get("/menu_item", response_model=Union[Error, List[MenuItemsOut]], tags=["Menu Items"])
 def list_menu_items(
     repo: MenuItemsRepository = Depends(),
 ):
@@ -26,7 +26,7 @@ def list_menu_items(
 
 
 @router.put(
-    "/menu_item/{menu_item_id}", response_model=Union[MenuItemOut, Error], tags=["Menu Items"]
+    "/menu_item/{menu_item_id}", response_model=Union[MenuItemsOut, Error], tags=["Menu Items"]
 )
 def update_menu_item(
     menu_item_id: int,
@@ -37,7 +37,7 @@ def update_menu_item(
 
 
 @router.get(
-    "/menu_item/{menu_item_id}", response_model=Union[MenuItemOut, Error], tags=["Menu Items"]
+    "/menu_item/{menu_item_id}", response_model=Union[MenuItemsOut, Error], tags=["Menu Items"]
 )
 def get_menu_item(
     menu_item_id=int,
