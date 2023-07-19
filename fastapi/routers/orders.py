3b3707b5f016d148abf1f2a7ac78731df72295b3
Edward Lee <eleet94@gmail.com>
2023-07-19 19:01:15 +0000

    Revert "testing deployment"

--- fastapi/routers/orders.py
+++ fastapi/routers/orders.py
@@ -1,57 +1,43 @@
 from fastapi import APIRouter, Depends
 from typing import Union, List
-from queries.orders import OrderIn, OrderOut, OrderRepository, Error
+from queries.orders import (OrderIn, OrderOut, OrderRepository, Error)
 
 
 router = APIRouter()
 
 
-@router.post("/order", response_model=Union[OrderOut, Error], tags=["Orders"])
-def create_order(orders: OrderIn, repo: OrderRepository = Depends()):
-    return repo.create(orders)
+@router.post("/order", response_model=Union[OrderOut, Error])
+def create_order(order: OrderIn, repo: OrderRepository = Depends()):
+    return repo.create(order)
 
 
-@router.get(
-    "/order/{orders_id}",
-    response_model=Union[Error, OrderOut],
-    tags=["Orders"],
-)
+@router.get("/order/{order_id}", response_model=Union[Error, OrderOut])
 def get_detail_order(
-    orders_id: int,
-    repo: OrderRepository = Depends(),
+        order_id: int,
+        repo: OrderRepository = Depends(),
 ) -> OrderOut:
-    return repo.get_order(orders_id)
+    return repo.get_order(order_id)
 
 
-@router.get(
-    "/order", response_model=Union[Error, List[OrderOut]], tags=["Orders"]
-)
+@router.get("/orders", response_model=Union[Error, List[OrderOut]])
 def list_orders(
     repo: OrderRepository = Depends(),
 ):
-    return repo.get_orders()
+    return repo.list_orders()
 
 
-@router.put(
-    "/order/{orders_id}",
-    response_model=Union[OrderOut, Error],
-    tags=["Orders"],
-)
+@router.put("/order/{order_id}", response_model=Union[OrderOut, Error])
 def update_order(
-    orders_id: int,
-    orders: OrderIn,
+    order_id: int,
+    order: OrderIn,
     repo: OrderRepository = Depends(),
 ) -> Union[OrderOut, Error]:
-    return repo.update(orders_id, orders)
+    return repo.update(order_id, order)
 
 
-@router.delete(
-    "/order/{orders_id}",
-    response_model=Union[OrderOut, Error],
-    tags=["Orders"],
-)
+@router.delete("/order/{order_id}", response_model=Union[OrderOut, Error])
 def delete_order(
-    orders_id: int,
+    order_id: int,
     repo: OrderRepository = Depends(),
 ) -> bool:
-    return repo.delete(orders_id)
+    return repo.delete(order_id)
