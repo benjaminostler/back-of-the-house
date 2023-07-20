8ab72fd2e792b7f8d08b882a8885707f004d47b2
Ed Lee <eleet94@gmail.com>
2023-07-19 21:36:39 -0700

    more deployment stuff

--- fastapi/queries/pool.py
+++ fastapi/queries/pool.py
@@ -2,5 +2,6 @@ import os
 from psycopg_pool import ConnectionPool
 
 pool = ConnectionPool(
-    conninfo=os.environ["postgresql://gastroids_user:password@db/gastroids_db"]
+    conninfo=os.environ
+    ["postgresql://gastroids_user:password@localhost:8000/gastroids_db"]
     )
