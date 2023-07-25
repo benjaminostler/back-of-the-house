d244ef739ba04eb91bf505bf88842503cce66087
Ed Lee <eleet94@gmail.com>
2023-07-25 10:23:48 -0700

    added RTK to dependencies, created counter feature, created cart feature (WIP)

---
+++ ghi/src/features/counter/Counter.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+  incrementAsync,
+  selectCount,
+} from "./CounterSlice";
+import { addToCart, removeFromCart } from "../cart/CartSlice";
+
+export function Counter() {
+  const count = useSelector(selectCount);
+  const dispatch = useDispatch();
+  const [incrementAmount, setIncrementAmount] = useState("2");
+
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          +
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          -
+        </button>
+      </div>
+      <div>
+        <input
+          aria-label="Set increment amount"
+          value={incrementAmount}
+          onChange={(e) => setIncrementAmount(e.target.value)}
+        />
+        <button
+          onClick={() =>
+            dispatch(incrementByAmount(Number(incrementAmount) || 0))
+          }
+        >
+          Enter Quantity
+        </button>
+        <button onClick={() => dispatch(addToCart)}>Add to cart</button>
+        <button onClick={() => dispatch(removeFromCart)}>
+          Remove from cart
+        </button>
+      </div>
+    </div>
+  );
+}
