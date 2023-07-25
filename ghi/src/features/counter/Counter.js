be0ad530f43686fabb021cb77aebf8e15d380139
Ed Lee <eleet94@gmail.com>
2023-07-25 10:28:08 -0700

    added RTK to dependencies, created counter feature, created cart feature (WIP)

--- ghi/src/features/counter/Counter.js
+++ ghi/src/features/counter/Counter.js
@@ -4,7 +4,6 @@ import {
   decrement,
   increment,
   incrementByAmount,
-  incrementAsync,
   selectCount,
 } from "./CounterSlice";
 import { addToCart, removeFromCart } from "../cart/CartSlice";
