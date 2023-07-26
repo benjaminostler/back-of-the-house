75bed7d99cd585c19d38100ab03718f3637e232c
Alec <aweinstein94@gmail.com>
2023-07-26 11:38:04 -0700

    Menu Page, Menu Item detail, index.html, working bootstrap, changes to navbar in app

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -5,11 +5,33 @@ import Nav from "./Nav.js";
 import SignupForm from "./accounts/SignupForm.js";
 import LoginForm from "./accounts/LoginForm.js";
 import { AuthProvider } from "@galvanize-inc/jwtdown-for-react";
-
+import Menu from "./menu/Menu.js";
+import MenuItemDetail from "./menu/MenuItemDetail.js";
 
 function App() {
   const domain = /https:\/\/[^/]+/;
-	const basename = process.env.PUBLIC_URL.replace(domain, '');
+  const basename = process.env.PUBLIC_URL.replace(domain, "");
+
+  const [menuItems, setMenuItems] = useState([]);
+  // const[selectedmenuItem, setselectedMenuItem]=useState("");
+
+  async function getMenuItems() {
+    const url = `http://localhost:8000/menu_items/`;
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      // console.log("menus", data);
+      // console.log("data object", data[0]);
+      // console.log("!!!!", data[0].picture_url);
+      setMenuItems(data);
+    } else {
+      console.error(response);
+    }
+  }
+
+  useEffect(() => {
+    getMenuItems();
+  }, []);
 
   return (
     <BrowserRouter basename={basename}>
@@ -17,13 +39,14 @@ function App() {
         <Nav />
         <div className="container">
           <Routes>
-
-
             <Route path="/" element={<MainPage />} />
             <Route path="/accounts/new" element={<SignupForm />} />
             <Route path="/loginform" element={<LoginForm />} />
-
-
+            <Route path="/menu">
+              <Route index element={<Menu menuItems={menuItems} />} />
+              {/* <Route path="/{menu_item_id}" OR /:id  element={<MenuItemDetails />} /> */}
+              <Route path=":id" element={<MenuItemDetail />} />
+            </Route>
           </Routes>
         </div>
       </AuthProvider>
