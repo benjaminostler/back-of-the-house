873d58fcae8dc3783a2707e343c968fb10cfffd1
Ed Lee <eleet94@gmail.com>
2023-07-25 19:56:56 -0700

    testing unit tests 4

--- fastrapi/queries/pool.py
+++ fastrapi/queries/pool.py
@@ -1,6 +1,4 @@
 import os
 from psycopg_pool import ConnectionPool
 
-pool = ConnectionPool(
-    conninfo=os.environ["postgresql://gastroids_user:password@db/gastroids_db"]
-)
+pool = ConnectionPool(conninfo=os.environ["DATABASE_URL"])
