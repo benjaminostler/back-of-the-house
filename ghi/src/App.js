d67614632970ba4635e670b8fd351d6d78af92ec
Benjamin Ostler <benjaminostler@gmail.com>
2023-07-26 09:42:25 -0700

    added create menu item page, added to journal

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -5,25 +5,45 @@ import Nav from "./Nav.js";
 import SignupForm from "./accounts/SignupForm.js";
 import LoginForm from "./accounts/LoginForm.js";
 import { AuthProvider } from "@galvanize-inc/jwtdown-for-react";
-
+import Menu from "./menu/Menu.js";
+// import MenuItemDetails from "./menu/MenuItemDetails.js";
+import MenuItemForm from "./menu/MenuItemForm.js";
 
 function App() {
   const domain = /https:\/\/[^/]+/;
-	const basename = process.env.PUBLIC_URL.replace(domain, '');
+  const basename = process.env.PUBLIC_URL.replace(domain, "");
 
+  const [menuItems, setMenuItems] = useState([]);
+  // const [menuItem, setMenuItem] = useState('');
+
+  async function getMenuItems() {
+    const url = "http://localhost:8000/menu_items/";
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      setMenuItems(data);
+    } else {
+      console.error(response);
+    }
+  }
+
+  useEffect(() => {
+    getMenuItems();
+  }, []);
   return (
     <BrowserRouter basename={basename}>
       <AuthProvider baseUrl={process.env.REACT_APP_API_HOST}>
         <Nav />
         <div className="container">
           <Routes>
-
-
             <Route path="/" element={<MainPage />} />
             <Route path="/accounts/new" element={<SignupForm />} />
             <Route path="/loginform" element={<LoginForm />} />
-
-
+            <Route path="menu">
+              <Route index element={<Menu menuItems={menuItems}/>}/>
+              {/* <Route path="/details" element={<MenuItemDetails menuItems={menuItems} />} /> */}
+              <Route path="new" element={<MenuItemForm />} />
+            </Route>
           </Routes>
         </div>
       </AuthProvider>
--- ghi/src/Nav.js
+++ ghi/src/Nav.js
@@ -1,34 +1,31 @@
 import { NavLink, useNavigate } from "react-router-dom";
-import { useState, useEffect } from 'react';
-import useToken from '@galvanize-inc/jwtdown-for-react';
-
+import { useState, useEffect } from "react";
+import useToken from "@galvanize-inc/jwtdown-for-react";
 
 function Nav() {
-
-  const {token, logout} = useToken();
+  const { token, logout } = useToken();
   const [showLogin, setShowLogin] = useState(true);
-  console.log(token)
+  console.log(token);
 
-	const handleLogout = () => {
-		logout();
-		setShowLogin(true);
+  const handleLogout = () => {
+    logout();
+    setShowLogin(true);
     // setShowBanner(true);
-	};
+  };
 
-	const checkForToken = () => {
-		if (token) {
-			setShowLogin(false);
-		} else {
+  const checkForToken = () => {
+    if (token) {
+      setShowLogin(false);
+    } else {
       setShowLogin(true);
     }
-	};
+  };
 
-	useEffect(() => {
-		checkForToken();
-	}, [token]);
+  useEffect(() => {
+    checkForToken();
+  }, [token]);
 
-  console.log('Token:', token);
-
+  console.log("Token:", token);
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-success">
@@ -74,6 +71,12 @@ function Nav() {
                 </NavLink>
               </li>
             )}
+
+            <li className="nav-item">
+              <NavLink className="nav-link" to="menu/new">
+                Create Menu Item
+              </NavLink>
+            </li>
           </ul>
         </div>
       </div>
