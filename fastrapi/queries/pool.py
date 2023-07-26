9d0b8c9b751802aba2453a8f6a80b25f7326b30d
Ed Lee <eleet94@gmail.com>
2023-07-25 19:43:47 -0700

    testing unit tests 2

--- fastrapi/queries/pool.py
+++ fastrapi/queries/pool.py
@@ -1,4 +1,6 @@
 import os
 from psycopg_pool import ConnectionPool
 
-pool = ConnectionPool(conninfo=os.environ["DATABASE_URL"])
+pool = ConnectionPool(
+    conninfo=os.environ["https://mar-2-pt-fastrapi.mod3projects.com/"]
+)
