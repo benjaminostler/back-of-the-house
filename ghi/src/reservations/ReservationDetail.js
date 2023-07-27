15521a6479211bfb0ebc404238fb07058797ecb6
Ed Lee <eleet94@gmail.com>
2023-07-26 18:19:52 -0700

    moved counter into cart, added CORS headers, fixed reservation detail es lint error

--- ghi/src/reservations/ReservationDetail.js
+++ ghi/src/reservations/ReservationDetail.js
@@ -1,35 +1,36 @@
-import { useParams } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useParams } from "react-router-dom";
+import { useEffect, useState, useCallback } from "react";
 
 export default function Reservation() {
-    const { id } = useParams();
-    const [reservation, setReservation] = useState();
+  const { id } = useParams();
+  const [reservation, setReservation] = useState();
 
-    const fetchReservationData = async () => {
-        const url = 'http://localhost:8000/reservations/' + id;
-        const response = await fetch(url)
+  const fetchReservationData = useCallback(async () => {
+    const url = "http://localhost:8000/reservations/" + id;
+    const response = await fetch(url);
 
-        if(response.ok) {
-            const data = await response.json();
-            setReservation(data)
-        }
+    if (response.ok) {
+      const data = await response.json();
+      setReservation(data);
     }
+  }, [id]); // Include 'id' as a dependency to appease es linter
 
-    useEffect(() => {
-        fetchReservationData();
-    }, [])
+  useEffect(() => {
+    fetchReservationData();
+  }, [fetchReservationData]); // Include 'fetchReservationData' as a dependency
+}
 
-    // useEffect(() => {
-    //     console.log('useEffect');
-    //     const url = 'http://localhost:8000/reservations/' + id;
-    //     fetch(url)
-    //         .then((response) => {
-    //             return response.json();
-    //         })
-    //         .then((data) => {
-    //             setReservation(data);
-    //         })
-    // })
+// useEffect(() => {
+//     console.log('useEffect');
+//     const url = 'http://localhost:8000/reservations/' + id;
+//     fetch(url)
+//         .then((response) => {
+//             return response.json();
+//         })
+//         .then((data) => {
+//             setReservation(data);
+//         })
+// })
 
 //     return (
 //         <>
@@ -43,35 +44,34 @@ export default function Reservation() {
 //     )
 // }
 
-    return (
-        <div>
-            <h1 className="mt-3 mb-3 p-0">Reservations</h1>
-            <table className="table table-striped">
-                <thead>
-                    <tr>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Email</th>
-                        <th>Phone Number</th>
-                        <th>Party Size</th>
-                        <th>Date</th>
-                        <th>Time</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {reservation ? (
-                        <tr>
-                            <td>{reservation.first_name}</td>
-                            <td>{reservation.last_name}</td>
-                            <td>{reservation.email}</td>
-                            <td>{reservation.phone_number}</td>
-                            <td>{reservation.party_size}</td>
-                            <td>{reservation.date}</td>
-                            <td>{reservation.time}</td>
-                        </tr>
-                    ) : null}
-                </tbody>
-            </table>
-        </div>
-    )
-}
+return (
+  <div>
+    <h1 className="mt-3 mb-3 p-0">Reservations</h1>
+    <table className="table table-striped">
+      <thead>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Email</th>
+          <th>Phone Number</th>
+          <th>Party Size</th>
+          <th>Date</th>
+          <th>Time</th>
+        </tr>
+      </thead>
+      <tbody>
+        {reservation ? (
+          <tr>
+            <td>{reservation.first_name}</td>
+            <td>{reservation.last_name}</td>
+            <td>{reservation.email}</td>
+            <td>{reservation.phone_number}</td>
+            <td>{reservation.party_size}</td>
+            <td>{reservation.date}</td>
+            <td>{reservation.time}</td>
+          </tr>
+        ) : null}
+      </tbody>
+    </table>
+  </div>
+);
