d2196b7fa34004c262db50cc3ec60d3addd2512e
Alec <aweinstein94@gmail.com>
2023-07-27 10:14:09 -0700

    Updated App.js

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -4,10 +4,15 @@ import Nav from "./Nav.js";
 import SignupForm from "./accounts/SignupForm.js";
 import LoginForm from "./accounts/LoginForm.js";
 import { AuthProvider } from "@galvanize-inc/jwtdown-for-react";
+import ReservationForm from "./reservations/ReservationForm.js";
+import ReservationList from "./reservations/ReservationList.js";
+import ReservationDetail from "./reservations/ReservationDetail.js";
+import Cart from "./Cart.js";
 import MenuItemForm from "./menu/MenuItemForm.js";
 import Menu from "./menu/Menu.js";
 import MenuItemDetail from "./menu/MenuItemDetail.js";
 import { useState, useEffect } from "react";
+
 function App() {
   const domain = /https:\/\/[^/]+/;
   const basename = process.env.PUBLIC_URL.replace(domain, "");
@@ -15,13 +20,10 @@ function App() {
   const [menuItems, setMenuItems] = useState([]);
 
   async function getMenuItems() {
-    const url = `http://localhost:8000/menu_items/`;
+    const url = `${process.env.REACT_APP_API_HOST}/menu_items/`;
     const response = await fetch(url);
     if (response.ok) {
       const data = await response.json();
-      // console.log("menus", data);
-      // console.log("data object", data[0]);
-      // console.log("!!!!", data[0].picture_url);
       setMenuItems(data);
     } else {
       console.error(response);
@@ -41,7 +43,10 @@ function App() {
             <Route path="/" element={<MainPage />} />
             <Route path="/accounts/new" element={<SignupForm />} />
             <Route path="/loginform" element={<LoginForm />} />
-
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/reservations/new" element={<ReservationForm />} />
+            <Route path="/reservations" element={<ReservationList />} />
+            <Route path="/reservations/:id" element={<ReservationDetail />} />
             <Route path="/menu_items">
               <Route index element={<Menu menuItems={menuItems} />} />
               <Route path=":id" element={<MenuItemDetail />} />
