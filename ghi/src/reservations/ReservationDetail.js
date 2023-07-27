1abbbd870e03d2dd4585d90f29354062e823ab93
Ed Lee <eleet94@gmail.com>
2023-07-26 18:24:14 -0700

    moved counter into cart, added CORS headers, fixed reservation detail es lint error

--- ghi/src/reservations/ReservationDetail.js
+++ ghi/src/reservations/ReservationDetail.js
@@ -18,60 +18,60 @@ export default function Reservation() {
   useEffect(() => {
     fetchReservationData();
   }, [fetchReservationData]); // Include 'fetchReservationData' as a dependency
-}
 
-// useEffect(() => {
-//     console.log('useEffect');
-//     const url = 'http://localhost:8000/reservations/' + id;
-//     fetch(url)
-//         .then((response) => {
-//             return response.json();
-//         })
-//         .then((data) => {
-//             setReservation(data);
-//         })
-// })
+  // useEffect(() => {
+  //     console.log('useEffect');
+  //     const url = 'http://localhost:8000/reservations/' + id;
+  //     fetch(url)
+  //         .then((response) => {
+  //             return response.json();
+  //         })
+  //         .then((data) => {
+  //             setReservation(data);
+  //         })
+  // })
 
-//     return (
-//         <>
-//             {reservation ? (
-//                 <div>
-//                     <p>{reservation.id}</p>
-//                     <p>{reservation.first_name}</p>
-//                 </div>
-//             ) : null}
-//         </>
-//     )
-// }
+  //     return (
+  //         <>
+  //             {reservation ? (
+  //                 <div>
+  //                     <p>{reservation.id}</p>
+  //                     <p>{reservation.first_name}</p>
+  //                 </div>
+  //             ) : null}
+  //         </>
+  //     )
+  // }
 
-return (
-  <div>
-    <h1 className="mt-3 mb-3 p-0">Reservations</h1>
-    <table className="table table-striped">
-      <thead>
-        <tr>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Email</th>
-          <th>Phone Number</th>
-          <th>Party Size</th>
-          <th>Date</th>
-          <th>Time</th>
-        </tr>
-      </thead>
-      <tbody>
-        {reservation ? (
+  return (
+    <div>
+      <h1 className="mt-3 mb-3 p-0">Reservations</h1>
+      <table className="table table-striped">
+        <thead>
           <tr>
-            <td>{reservation.first_name}</td>
-            <td>{reservation.last_name}</td>
-            <td>{reservation.email}</td>
-            <td>{reservation.phone_number}</td>
-            <td>{reservation.party_size}</td>
-            <td>{reservation.date}</td>
-            <td>{reservation.time}</td>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Phone Number</th>
+            <th>Party Size</th>
+            <th>Date</th>
+            <th>Time</th>
           </tr>
-        ) : null}
-      </tbody>
-    </table>
-  </div>
-);
+        </thead>
+        <tbody>
+          {reservation ? (
+            <tr>
+              <td>{reservation.first_name}</td>
+              <td>{reservation.last_name}</td>
+              <td>{reservation.email}</td>
+              <td>{reservation.phone_number}</td>
+              <td>{reservation.party_size}</td>
+              <td>{reservation.date}</td>
+              <td>{reservation.time}</td>
+            </tr>
+          ) : null}
+        </tbody>
+      </table>
+    </div>
+  );
+}
