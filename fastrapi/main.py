1035ad9b23983e9759503bd67f856b4432ad8856
Kenny Phung <kenny.phung12@gmail.com>
2023-07-27 14:14:35 -0700

    Login/Signup Fixed

--- fastrapi/main.py
+++ fastrapi/main.py
@@ -23,6 +23,16 @@ app.include_router(cart.router)
 app.include_router(cart_items.router)
 # changed to allow deployment database to make CORS requests
 
+origins = [os.environ.get("CORS_HOST", None), "http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # app.add_middleware(
 #     CORSMiddleware,
 #     allow_origins=[
@@ -37,20 +47,20 @@ app.include_router(cart_items.router)
 # )
 
 
-origins = [
-    os.environ.get("CORS_HOST", "http://localhost:3000"),
-    os.environ.get("CORS_HOST", "http://localhost:8000"),
-    os.environ.get("CORS_HOST", "https://mar-2-pt-fastrapi.mod3projects.com"),
-    os.environ.get("CORS_HOST", "https://backofthehouse.gitlab.io/"),
-]
+# origins = [
+#     os.environ.get("CORS_HOST", "http://localhost:3000"),
+#     os.environ.get("CORS_HOST", "http://localhost:8000"),
+#     os.environ.get("CORS_HOST", "https://mar-2-pt-fastrapi.mod3projects.com"),
+#     os.environ.get("CORS_HOST", "https://backofthehouse.gitlab.io/"),
+# ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 
 @app.get("/")
