dcf67f3479530e535aff8f5694d404dcb4738319
Kenny Phung <kenny.phung12@gmail.com>
2023-07-28 05:49:26 -0700

    Login/Signup is fixed again for prod, navLink item 'My Account' fixed

---
+++ fastrapi/tests/test_accounts.py
@@ -0,0 +1,26 @@
+from fastapi.testclient import TestClient
+from main import app
+from queries.accounts import AccountRepository
+
+client = TestClient(app)
+
+totally_real_and_not_fake_db = [
+    {
+        "id": 1,
+        "first_name": "Penny",
+        "last_name": "Khung",
+        "username": "lookinhungry",
+        "hash_password": "8asd5g1ga2sdgs5we8g1",
+        "email": "pkhung@gmail.com",
+        "phone_number": "888-888-8888",
+    },
+    {
+        "id": 2,
+        "first_name": "Pat",
+        "last_name": "Fusty",
+        "username": "justabite",
+        "hash_password": "8asd5g1ga2sdgs5we8g1",
+        "email": "pkhung@gmail.com",
+        "phone_number": "888-888-8888",
+    },
+]
