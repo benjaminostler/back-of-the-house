87b24634d917a2192a3fba97951a264ffde59dc3
Benjamin Ostler <benjaminostler@gmail.com>
2023-07-28 14:17:53 -0700

    removed duplicate text in nav bar

--- ghi/src/Nav.js
+++ ghi/src/Nav.js
@@ -31,7 +31,6 @@ function Nav() {
       <div className="container-fluid">
         <NavLink className="navbar-brand" to="/">
           Back of the House
-          Back of the House
         </NavLink>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
