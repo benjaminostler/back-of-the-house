ca46be60ae12e6f48514947551240a052ce5bf09
Benjamin Ostler <benjaminostler@gmail.com>
2023-07-28 18:14:40 -0700

    added nav bar dropdowns & removed extra code in carts.js

--- ghi/src/Cart.js
+++ ghi/src/Cart.js
@@ -2,7 +2,6 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect, useState } from "react";
 import {
-  addToCart,
   incrementQuantity,
   decrementQuantity,
   enterQuantity,
